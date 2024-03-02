495b28003b2f0151467e40a835ad8b313907e6b4
harshit7861 <157456195+harshit7861@users.noreply.github.com>
2024-03-02 22:15:05 +0530

    Create fibonnaci.py

---
+++ fibonnaci.py
@@ -0,0 +1,8 @@
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Print the first 100 Fibonacci numbers
+for i in range(100):
+# Print the first 15 Fibonacci numbers
+for i in range(15):
+    print(fibonacci(i))
